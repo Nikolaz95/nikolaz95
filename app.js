5cfd2e6c2b50e22c6301a6a90f606bd2f842d4db
Nikola Zovko <nikolajoe95@gmail.com>
2023-08-16 22:31:07 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+
+/* kod za hambgurger menu  */
+const hamburger = document.querySelector(".ham");  
+const navsub = document.querySelector(".navbar");  
+hamburger.addEventListener('click', () => {  
+ hamburger.classList.toggle("change")  
+ navsub.classList.toggle("nav-change")  
+});  
