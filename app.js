406bcf58d8d52d3f926cc72af08f4fed1d1780d5
Nikola Zovko <nikolajoe95@gmail.com>
2023-08-19 15:13:18 +0200

    new-update2

--- app.js
+++ app.js
@@ -6,3 +6,29 @@ hamburger.addEventListener('click', () => {
  hamburger.classList.toggle("change")  
  navsub.classList.toggle("nav-change")  
 });  
+
+
+/* work popup kod  */
+
+
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("work-button")) {
+        togglePortfolioPopup();
+        porfolioItemDetails(e.target.parentElement);
+    }
+})
+
+function togglePortfolioPopup () {
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio-popup-close").addEventListener("click", togglePortfolioPopup)
+
+
+function porfolioItemDetails(portfolioItem) {
+    document.querySelector("pp-thumnail").src = portfolioItem.querySelector(".work-img").src;
+
+    document.querySelector(".portfolio-popup-subtitle span").innerHTML = portfolioItem.querySelector(".work-title").innerHTML;
+
+    document.querySelector(".portfolio-popup-body").innerHTML = portfolioItem.querySelector(".details-title").innerHTML;
+}
